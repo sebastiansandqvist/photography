0895684ab14482f5003db33572005fd3c4fb07a0
Sebastian Sandqvist <seb@sparque.me>
2015-09-17 13:17:22 -0700

    setup

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
 const react = require('react');
+
+class App extends react.Component {
+
+	render() {
+		return (
+			<div>
+				Hello world!
+			</div>
+		);
+	}
+
+}
+
+react.render(<App/>, document.body);
---
+++ app/styles/constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+	
+	color: {
+		bg: '#111',
+		orange: '#f42'
+	}/*,
+
+	type: {
+		letterSpacing: 8,
+		margin: 0,
+		padding: 0,
+		font: '400 20px/20px "Open Sans"',
+		textAlign: 'center',
+		textIndent: 8,
+		textTransform: 'uppercase'
+	},
+
+	sep: {
+		...
+	}
+*/
+}
